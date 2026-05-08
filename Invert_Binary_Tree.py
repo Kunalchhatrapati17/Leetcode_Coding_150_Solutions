9d2fa8b5803eb27fc561ca926a630d1bd0aae32f
Kunal Chhatrapati <54681555+Kunalchhatrapati17@users.noreply.github.com>
2026-05-07 18:55:00 -0700

    Add invertTree method to invert binary tree
    Implement the invertTree method to invert a binary tree.

---
+++ Invert_Binary_Tree.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: Optional[TreeNode]
+        """
+                # Base case: if the node is None, return None
+        if root is None:
+            return None
+
+        # Recursively invert the left and right subtrees
+        inverted_left = self.invertTree(root.left)
+        inverted_right = self.invertTree(root.right)
+
+        # Swap the left and right children of the current node
+        root.left = inverted_right
+        root.right = inverted_left
+
+        # Return the root node with its children swapped
+        return root
