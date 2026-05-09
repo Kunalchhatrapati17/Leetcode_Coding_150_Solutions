fef56d54ddd1e41e24623cd07e1b94cfabb5a03c
Kunal Chhatrapati <54681555+Kunalchhatrapati17@users.noreply.github.com>
2026-05-08 18:27:19 -0700

    Create Kth_Larget_Element_Array.py

---
+++ Kth_Larget_Element_Array.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        return self.quick_select(nums, len(nums) - k)
+
+    def quick_select(self, nums, k_smallest): # k_smallest (0-based index)
+        pivot = random.choice(nums)
+        left, mid, right = [], [], []
+
+        for num in nums:
+            if num == pivot:
+                mid.append(num)
+            elif num < pivot:
+                left.append(num)
+            else:
+                right.append(num)
+        
+        if k_smallest < len(left):  # k_smallest is on the left side
+            return self.quick_select(left, k_smallest)
+
+        if k_smallest < len(left) + len(mid):  # k_smallest is on the middle
+            return pivot
+
+        return self.quick_select(right, k_smallest - len(left) - len(mid))
