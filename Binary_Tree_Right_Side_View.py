7873f0860379c8a6c46ca239e67211d53d80a42d
Kunal Chhatrapati <54681555+Kunalchhatrapati17@users.noreply.github.com>
2026-05-12 22:18:34 -0700

    Implement right side view of binary tree

---
+++ Binary_Tree_Right_Side_View.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+class Solution(object):
+    def rightSideView(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+        if not root:
+            return []
+
+        result = []
+        queue = deque([root])
+
+        while queue:
+            level_size = len(queue)
+
+            for i in range(level_size):
+                node = queue.popleft()
+
+                # Last node in the level = rightmost visible node
+                if i == level_size - 1:
+                    result.append(node.val)
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+        return result
